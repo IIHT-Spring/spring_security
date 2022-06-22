7c2ed102b6ce0ed81fa38ed549a3bf1175bb4987
ngactive99 <ngactive99@gmail.com>
2022-06-22 13:54:55 +0530

    created token

--- src/main/java/com/secure/jwt/controller/WelcomeController.java
+++ src/main/java/com/secure/jwt/controller/WelcomeController.java
@@ -1,12 +1,42 @@
 package com.secure.jwt.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.secure.jwt.entity.AuthRequest;
+import com.secure.jwt.util.JwtUtil;
 
 @RestController
 public class WelcomeController {
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
 	@GetMapping("/")
 	public String Welcome() {
 		return "welcome to spring security sesssion";
+	}
+
+	@PostMapping("/authenticate")
+	public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword())
+
+			);
+		} catch (Exception ex) {
+			throw new Exception("Invalid username or password");
+		}
+
+		return jwtUtil.generateToken(authRequest.getUserName());
+
 	}
 }
